413e64a9df9669cb46b065ae47a221b22a5eb0cb
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-08-28 22:16:42 +0300

    Release 0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 
 setup(
     name='collective.behavior.salable',
-    version='0.0',
+    version='0.1',
     description="Behavior to make content salable.",
     long_description=long_description,
     # Get more strings from
